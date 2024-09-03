b004e7789ba0d7b9fe1cb9c7e68a3672696f3f45
kekelidiane <lucedia09@gmail.com>
2024-09-03 10:55:22 +0000

    jwt Auth sample

---
+++ routers.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+from functions import authenticate_user, create_access_token, fake_users_db, ACCESS_TOKEN_EXPIRE_MINUTES, get_current_active_user
+from models import Token, User
+from main import app
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username ou password incorrect",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    print(f"Jeton JWT: {access_token}")
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
