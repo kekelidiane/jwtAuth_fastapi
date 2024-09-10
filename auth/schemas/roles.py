16f40a98add8f2caa2de054655bf96ddb9e4c17e
kekelidiane <lucedia09@gmail.com>
2024-09-10 08:18:15 +0000

    8h18

--- auth/schemas/roles.py
+++ auth/schemas/roles.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel, EmailStr, Field
-from typing import List, Optional
+from pydantic import BaseModel, Field
+from enum import Enum
 
+class DroitEnum(Enum):
+    CONFIGURER_SITE = "configurer le site"
+    PUBLIER_ANNONCES = "publier des annonces"
+    SUPPRIMER_ANNONCES = "supprimer des annonces"
 
 class Role(BaseModel):
     id: str
